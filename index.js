82efeaa14664f9bc73395dc3162edaa39a93b250
Rabi Alaya <rabialaya@gmail.com>
2022-06-01 01:12:24 -0500

    added hover effect on each div

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 // create a 16x16 grid and place in grid container
 const grid_container = document.querySelector('.grid_container');
 
-for (let i = 0; i < (16); i++) {
-    const div = document.createElement('div');
-    grid_container.appendChild(div);
+for (let i = 0; i < 16; i++) {
+    const grid_row = document.createElement('div');
+    grid_row.style.display = 'flex';
+    grid_container.appendChild(grid_row)
+    for (let j = 0; j < 16; j++) {
+        const div = document.createElement('div');
+        div.textContent = "DIV";
+        div.classList.add('grid_item');
+        grid_row.appendChild(div);
+    }
 }
+
+const grid_items = document.querySelectorAll('.grid_item');
+grid_items.forEach((grid_item) => {
+
+    // and for each one we add a 'click' listener
+    grid_item.addEventListener('mouseover', function (e) {
+      e.target.style.background = 'blue';
+    });
+});
