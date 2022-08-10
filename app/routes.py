b904437d2d42ed8eb9274d392591d2bc4a1fedb2
Rabi Alaya <rabialaya@gmail.com>
2022-08-10 16:28:28 -0500

    converted to flask app

--- app/routes.py
+++ app/routes.py
@@ -2,5 +2,5 @@ from app import app
 from flask import render_template, url_for
 
 @app.route('/', methods=['Get', 'POST'])
-def main():
-    return render_template(url_for('index'))
+def index():
+    return render_template('index.html')
