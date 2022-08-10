b1059186dc7c2a3e0d269cca719eab341f3b4fc4
Rabi Alaya <rabialaya@gmail.com>
2022-08-10 17:10:16 -0500

    update

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,10 @@
 from app import app
-from flask import render_template, url_for
+from flask import render_template
 
 @app.route('/', methods=['Get', 'POST'])
 def index():
     return render_template('index.html')
+
+@app.route('/explore', methods=['GET', 'POST'])
+def explore():
+    return render_template('explore.html')
